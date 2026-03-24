ba5b9d6526aa8ba18e4a52465e946884e9f44e1a
revisionartslab <revisionartslab@gmail.com>
2026-03-24 23:34:17 +0900

    Auto-published gallery updates from CLOUDFLOW

--- gallery/gallery-logic.js
+++ gallery/gallery-logic.js
@@ -382,6 +382,20 @@ const STREAM_RECORDS = [
         "uploadedAt": "2026-03-24T14:32:04Z"
     },
     {
+        "id": "master 20260323 180210 [FPS 16x9]",
+        "tags": [
+            "Graphics",
+            "layout",
+            "plane"
+        ],
+        "title": "master 20260323 180210 [FPS 16x9]",
+        "url": "https://res.cloudinary.com/dy2gu58kz/image/upload/v1774362847/revision-arts/master%2020260323%20180210%20%5BFPS%2016x9%5D.png",
+        "description": "REVISION ARTS honors humanity’s artistic heritage by utilizing algorithmic intelligence as an alchemical tool to deconstruct and reconstruct aesthetic DNA. Within this horizon, traditional mediums and computational creations coexist without distinction, merging into a singular vision. We define these tools not as an imitation, but as a transformative instrument and an expanded gaze that crystallizes fleeting thoughts into eternal forms.",
+        "promptUrl": "https://res.cloudinary.com/dy2gu58kz/raw/upload/v1774362119/revision-arts/prompt_20260323_180210_%5BFPS_16x9%5D",
+        "assetId": "276f57d0d77eba184c8c996e194c851c",
+        "uploadedAt": "2026-03-24T14:34:07Z"
+    },
+    {
         "id": "master 20260323 180219 [FPS 9x16]",
         "tags": [
             "Graphics",
